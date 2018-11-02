344a97b5fd45edcc6cc518b6998e11e2dca4f143
Ayaka-Katsuragi <38163555+Ayaka-Katsuragi@users.noreply.github.com>
2018-11-02 03:30:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+	bot.user.setActivity("Managing Server", {type: "PLAYING"});
 	bot.user.setStatus('dnd');
 });
 
